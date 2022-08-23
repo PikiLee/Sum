a9cc900b448c27fa00ebb1babfd1052f9c669a97
luckypeppa <871565198@qq.com>
2022-08-23 19:57:15 +0800

    Now use swiper js

--- src/elements/BaseTab.vue
+++ src/elements/BaseTab.vue
@@ -1,14 +1,9 @@
 <template>
   <div
-    class="relative border rounded-md border-black flex bg-green-400 z-0 text-white"
-    ref="group"
+    class="relative border rounded-md border-black flex bg-green-400 z-0 text-slate-900 gap-0"
   >
-    <span
-      class="absolute bg-white -z-10 rounded-md shadow-2xl shadow-orange-400 active"
-      ref="active"
-    ></span>
     <button
-      class="w-1/4 grow-0 transition-all duration-500 ease-in delay-300 text-lg"
+      class="w-full grow-0 h-full rounded-md transition-all duration-200 ease-in delay-300 text-lg"
       :class="{ 'btn-active': activeIndex === index }"
       v-for="(tabTitle, index) in tabTitles"
       :key="tabTitle"
@@ -20,51 +15,25 @@
 </template>
 
 <script setup lang="ts">
-import { gsap } from "gsap";
-import { onMounted, ref } from "vue";
-
 interface Props {
   tabTitles: string[];
   activeIndex: number;
 }
 
-const props = defineProps<Props>();
+defineProps<Props>();
 
 const emit = defineEmits(["update:activeIndex"]);
 
 /**
  * Toggle Active Tab
  */
-const active = ref(null);
-const group = ref<HTMLDivElement | null>(null);
-
 function toggleActive(index: number) {
-  animateActive(index);
   emit("update:activeIndex", index);
 }
-
-// animation
-function animateActive(index: number) {
-  gsap.to(active.value, {
-    x: () => (group.value?.offsetWidth ?? 0) * 0.25 * index,
-    duration: 1,
-  });
-}
-
-onMounted(() => {
-  animateActive(props.activeIndex);
-});
 </script>
 
 <style lang="scss" scoped>
-.active {
-  width: 21%;
-  left: 2%;
-  top: 7%;
-  height: 86%;
-}
-
 .btn-active {
-  color: black;
+  @apply bg-green-700 border border-green-600 text-slate-100 font-bold shadow-inner shadow-green-900;
 }
 </style>
