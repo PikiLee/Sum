1096a77fa494e16e43dfb558fdde64e79aa9a525
luckypeppa <871565198@qq.com>
2022-08-24 11:06:49 +0800

    Fix a bug.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
 import { RouterView } from "vue-router";
 import AppNavVue from "./components/AppNav.vue";
-
-const navBar = ref<InstanceType<typeof AppNavVue> | null>(null);
-const wrapper = ref<HTMLDivElement | null>(null);
-onMounted(() => {
-  if (wrapper.value && navBar.value) {
-    wrapper.value.style.setProperty("--pb", `${navBar.value.height * 2}px`);
-  }
-});
 </script>
 
 <template>
-  <div class="container px-4 wrapper mx-auto" ref="wrapper">
-    <AppNavVue ref="navBar" />
+  <div class="container px-4 wrapper mx-auto">
+    <AppNavVue />
     <AppNotis />
     <RouterView />
   </div>
   <ProgressBar />
 </template>
 
-<style scoped>
-.wrapper {
-  --pb: 10rem;
-  padding-bottom: var(--pb);
-}
-</style>
+<style scoped></style>
